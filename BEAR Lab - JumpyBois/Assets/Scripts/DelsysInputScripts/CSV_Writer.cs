e08a0c307e210eeb6948a80caf24d266fa044450
Josh McGinnis <17076535+vertig0ld@users.noreply.github.com>
2023-09-22 13:59:32 -0700

    Consolidated TCP_Connection scripts

--- BEAR Lab - JumpyBois/Assets/Scripts/DelsysInputScripts/CSV_Writer.cs
+++ BEAR Lab - JumpyBois/Assets/Scripts/DelsysInputScripts/CSV_Writer.cs
@@ -5,31 +5,36 @@ using System;
 using System.IO;
 using data;
 
-public class csv_writer
+public class CSV_Writer
 {
     private string fileDirectory;
-    private string filename;
+    private string fileName;
     string filePath;
 
-    public csv_writer()
+    public CSV_Writer()
     {
         fileDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        filename = "bear_data.csv";
-        filePath = Path.Combine(fileDirectory, filename);
+        fileName = "bear_data.csv";
+        filePath = Path.Combine(fileDirectory, fileName);
     }
 
-    public void writeHeaders(string headers)
+    public void WriteHeaders(string headers)
     {
         using (StreamWriter s = new StreamWriter(filePath, true))
         {
             s.WriteLine(headers);
         }
     }
-    public void writeDataPoint(DataPoint dp, String bufferValues, bool isTakingInput) {
+    public void WriteDataPoint(DataPoint dp, String bufferValues, bool isTakingInput) {
 
         using (StreamWriter s = new StreamWriter(filePath, true)) {
             s.WriteLine(dp.timeStamp + "," + bufferValues + "," + dp.majority + "," + isTakingInput);
         }
+        
+    }
+
+    public void CloseFile()
+    {
         //Look up how to close StreamWriter and when.
     }
 
--- BEAR Lab - JumpyBois/Assets/Scripts/DelsysInputScripts/ConnectionManager.cs
+++ BEAR Lab - JumpyBois/Assets/Scripts/DelsysInputScripts/ConnectionManager.cs
@@ -10,47 +10,50 @@ using System;
 using data;
 using System.Linq;
 
-public class ipc_connect: MonoBehaviour {
-    
-    TcpListener listener;
-    TcpClient client;
-    NetworkStream stream;
-    private Thread clientReceiveThread; 
-    private CSV_Writer writer;	
+public class ConnectionManager : MonoBehaviour
+{
+    private TcpListener listener;
+    private TcpClient client;
+    private NetworkStream stream;
+    private Thread clientReceiveThread;
+    private CSV_Writer writer;
     private int currMajority;
-    private int bufferSize = 5;
-    byte[] buffer;
+    [SerializeField] private int bufferSize;
+    private byte[] buffer;
 
-    private Queue<int> bufferQueue = new Queue<int>();
+    private Queue<int> bufferQueue;
 
     [SerializeField] private InputManager inputManager;
 
-    // Start is called before the first frame update
-    public void Start() { 
-        for(int i = 0; i < bufferSize; i++) {
-            addToBuffer(bufferQueue, 0);
+    private void Awake()
+    {
+        // if we are using keyboard input, disable this script
+        if (GlobalStorage.GameSettings.usingDelsys == false)
+            this.enabled = false;
+    }
+
+    private void Start()
+    {
+        bufferQueue = new Queue<int>(bufferSize);
+        ConnectToServer();
+    }
+
+    private void ConnectToServer()
+    {
+        try
+        {
+            // New thread will continuously listen for input
+            clientReceiveThread = new Thread(new ThreadStart(ListenForData));
+            clientReceiveThread.IsBackground = true;
+            clientReceiveThread.Start();
+        } catch (Exception e)
+        {
+            Debug.Log("On client connect exception " + e);
         }
-
-        connectToServer();
     }
 
-    private void OnApplicationQuit() {
-        Debug.Log("Application is quitting. Performing cleanup...");
-    }
-
-    public void connectToServer() {
-        try {  	
-			clientReceiveThread = new Thread(new ThreadStart(listenForData)); 			
-			clientReceiveThread.IsBackground = true; 			
-			clientReceiveThread.Start(); 
-		} 		
-		catch (Exception e) { 			
-			Debug.Log("On client connect exception " + e);
-		}
-    }
-
-
-    public void listenForData() {
+    private void ListenForData()
+    {
         IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
         int port = 1924;
 
@@ -62,79 +65,81 @@ public class ipc_connect: MonoBehaviour {
             listener.Start();
             Debug.Log("Waiting for connection...");
 
-            // Create a buffer to store received data
-            buffer = new byte[1024];
+            buffer = new byte[1024]; // Create a buffer to store received data
 
-            while(true) {
-                using(client = listener.AcceptTcpClient()) {
+            while (true)
+            {
+                using (client = listener.AcceptTcpClient())
+                {
                     Debug.Log("Client connected.");
-
-                    using(stream = client.GetStream()) {
+                    using (stream = client.GetStream())
+                    {
                         int length;
-                        while ((length = stream.Read(buffer, 0, buffer.Length)) != 0) {
-
+                        while ((length = stream.Read(buffer, 0, buffer.Length)) != 0)
+                        {
                             UnityMainThreadDispatcher.ExecuteOnMainThread(() =>
                             {
-                                var incommingData = new byte[length]; 							
-							    Array.Copy(buffer, 0, incommingData, 0, length);  							
-							
+                                var incommingData = new byte[length];
+                                Array.Copy(buffer, 0, incommingData, 0, length);
+
                                 // Convert byte array to string message. 							
-							    string clientMessage = Encoding.ASCII.GetString(incommingData);
-                                
+                                string clientMessage = Encoding.ASCII.GetString(incommingData);
+
                                 // Processes data in buffer and calculates majority
-                                if(clientMessage != null)
+                                if (clientMessage != null)
                                 {
                                     Debug.Log("client message received as: " + clientMessage);
-                                    addToBuffer(bufferQueue, int.Parse(clientMessage));
+                                    AddToBuffer(bufferQueue, int.Parse(clientMessage));
                                     Queue<int> tempQueue = bufferQueue;
-                                    currMajority = calculateMode(tempQueue);
+                                    currMajority = CalculateMode(tempQueue);
 
                                     // Creates a new DataPoint instance with the data calculated above and inputs into Delsys game control
-                                    DataPoint dataValue = new DataPoint(getCurrentTime(), calculateMode(tempQueue));
+                                    DataPoint dataValue = new DataPoint(GetCurrentTime(), CalculateMode(tempQueue));
                                     Debug.Log("The value is: " + dataValue.majority);
                                     inputManager.OnDelsysInput(dataValue.majority);
 
                                     // Records data to CSV
                                     writer.WriteHeaders("Timestamp, Buffer Values, Majority Output, Taking Input");
-                                    writer.WriteDataPoint(dataValue, getBufferContents(tempQueue), inputManager.enableInput);
+                                    writer.WriteDataPoint(dataValue, GetBufferContents(tempQueue), inputManager.enableInput);
                                     Debug.Log("time stamp: " + dataValue.timeStamp + ", majority: " + dataValue.majority + " ,is taking input: " + inputManager.enableInput);
                                 } else
                                 {
                                     Debug.Log("No data received");
-                                } 
+                                }
                             });
-                        } 
+                        }
                     }
                 }
             }
-        } catch (SocketException socketException) { 			
-			Debug.Log("SocketException " + socketException.ToString()); 
-		}   
+        } catch (SocketException socketException)
+        {
+            Debug.Log("SocketException " + socketException.ToString());
+        }
     }
 
-    private void addToBuffer(Queue<int> queue, int value) {
-        if (queue.Count >= 5) {
+    private void AddToBuffer(Queue<int> queue, int value)
+    {
+        if (queue.Count >= 5)
+        {
             queue.Dequeue();
         }
         queue.Enqueue(value);
 
-        getBufferContents(queue);
+        GetBufferContents(queue);
     }
 
-    private String getBufferContents(Queue<int> q) {
+    private String GetBufferContents(Queue<int> q)
+    {
         String consolePrintBuffer = "";
-        foreach (var item in q){
+        foreach (var item in q)
+        {
             consolePrintBuffer += " " + item;
         }
         return consolePrintBuffer;
     }
 
-    private String getCurrentTime() {
-        DateTime currentTime = DateTime.Now;
-        return currentTime.ToString("mm:ss:fff");
-    }
-
-    private int calculateMode(Queue<int> queue) {
+    private int CalculateMode(Queue<int> queue)
+    {
         Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
 
         Queue<int> tempQueue = new Queue<int>(queue);
@@ -152,4 +157,9 @@ public class ipc_connect: MonoBehaviour {
         return mode;
     }
 
+    private String GetCurrentTime()
+    {
+        DateTime currentTime = DateTime.Now;
+        return currentTime.ToString("mm:ss:fff");
+    }
 }
