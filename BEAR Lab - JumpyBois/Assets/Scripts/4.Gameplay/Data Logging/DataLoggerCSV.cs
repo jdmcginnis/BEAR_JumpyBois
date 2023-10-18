75645b047a637f284958bc8e877cf6c1716b9677
Josh McGinnis <17076535+vertig0ld@users.noreply.github.com>
2023-10-18 10:20:44 -0700

    Merged Lana's Changes
    CSV writer is now top notch

--- BEAR Lab - JumpyBois/Assets/Scripts/4.Gameplay/Data Logging/DataLoggerCSV.cs
+++ BEAR Lab - JumpyBois/Assets/Scripts/4.Gameplay/Data Logging/DataLoggerCSV.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.IO;
+using System.Linq;
 
 public class DataLoggerCSV : MonoBehaviour
 {
@@ -20,14 +21,45 @@ public class DataLoggerCSV : MonoBehaviour
 
     private void Start()
     {
-        fileDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        fileName = "bear_data.csv";
-        filePath = Path.Combine(fileDirectory, fileName);
+        string rootDirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "bearlab");
+        FindOrCreate(rootDirectoryPath);
+
+        string currentDate = DateTime.Now.ToString("MM-dd-yyyy");
+        string todaysDirPath = Path.Combine(rootDirectoryPath, currentDate);
+        FindOrCreate(todaysDirPath);
+        int fileCount = GetLatestFileCount(todaysDirPath, "*.csv") + 1;
+
+        fileName = fileCount + ".csv";
+        filePath = Path.Combine(todaysDirPath, fileName);
 
         csvHeader = "Timestamp, Buffer Values, Predicted Value, True Value, Accepting Input?";
 
         using (csvWriter = new StreamWriter(filePath, true))
             csvWriter.WriteLine(csvHeader);
+    }
+
+    // Checks if directory exists; if it doesn't, creates it
+    private void FindOrCreate(string path)
+    {
+        if (!File.Exists(path))
+            Directory.CreateDirectory(path);
+    }
+
+
+    private int GetLatestFileCount(string directoryPath, string filePattern)
+    {
+        string[] files = Directory.GetFiles(directoryPath, filePattern);
+        int largestNumber = 0;
+
+        if (files.Length > 0)
+        {
+            largestNumber = files
+                .Select(File => Path.GetFileNameWithoutExtension(File))
+                .Where(fileName => int.TryParse(fileName, out _))
+                .Select(fileName => int.Parse(fileName))
+                .Max();
+        }
+        return largestNumber;
     }
 
     private void FixedUpdate()
